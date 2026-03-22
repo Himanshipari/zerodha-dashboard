a0436a10af3fa1d4ff7800c84cc01e723f37ae1e
himanshi <pariharhimanshi02@gmail.com>
2026-03-22 14:37:54 +0530

    url

--- src/index.js
+++ src/index.js
@@ -1,16 +1,49 @@
-import React from "react";
+// import React from "react";
+// import ReactDOM from "react-dom/client";
+// import { BrowserRouter, Route, Routes } from "react-router-dom";
+// import "./index.css";
+// import Home from "./components/Home";
+
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+// root.render(
+//   <React.StrictMode>
+//     <BrowserRouter>
+//       <Routes>
+//         <Route path="/*" element={<Home />} />
+//       </Routes>
+//     </BrowserRouter>
+//   </React.StrictMode>
+// );
+
+
+import React, { useEffect } from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./index.css";
 import Home from "./components/Home";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-  <React.StrictMode>
+const App = () => {
+  useEffect(() => {
+    const params = new URLSearchParams(window.location.search);
+    const token = params.get("token");
+    if (token) {
+      localStorage.setItem("token", token);
+      window.history.replaceState({}, document.title, "/");
+    }
+  }, []);
+
+  return (
     <BrowserRouter>
       <Routes>
         <Route path="/*" element={<Home />} />
       </Routes>
     </BrowserRouter>
+  );
+};
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <App />
   </React.StrictMode>
 );
